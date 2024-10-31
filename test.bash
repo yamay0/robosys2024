88296b0c3cd271d82c7ae828ea58f4b963248c13
yamay0 <s22C1023BM@s.chibakoudai.jp>
2024-10-31 12:26:44 +0900

    第六回目のp20 実行してOKでないのは何だ

--- test.bash
+++ test.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ng () {
-	echo ${1}行目が違うyo
+	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=yamada
-[ "$a" = ueda ] || ng "$LINENO"
-[ "$a" = yamada ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
-
