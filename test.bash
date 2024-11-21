1f377b888520cd7cccf2006caef4fee368c231ab
yamay0 <s22C1023BM@s.chibakoudai.jp>
2024-11-21 13:40:36 +0900

    第六回 テスト項目を追加

--- test.bash
+++ test.bash
@@ -1,14 +1,25 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo ${1}行目が違うよ
+    res=1
 }
 
 res=0
 
+# 正常な入力の確認
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+# STRANGE INPUT
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
+
